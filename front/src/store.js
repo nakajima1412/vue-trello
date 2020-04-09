15b3d331d445b9a6fb3bafc4c291f14ea0143b28
nakajima1412 <nakajima1412@icloud.com>
2020-04-10 00:14:40 +0900

    add move task

--- front/src/store.js
+++ front/src/store.js
@@ -35,6 +35,10 @@ export default new Vuex.Store({
     },
     UPDATE_TASK (state, { task, key, value }) {
       task[key] = value
+    },
+    MOVE_TASK (state, { fromColumn, toColumn, taskIndex }) {
+      const taskToMove = fromColumn.splice(taskIndex, 1)[0]
+      toColumn.push(taskToMove)
     }
   }
 })
