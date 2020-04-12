82ba5c5952053835949ef9f85f74769e8fa0e920
nakajima1412 <nakajima1412@icloud.com>
2020-04-12 15:51:02 +0900

    新規カラム作成機能の追加

--- front/src/views/Board.vue
+++ front/src/views/Board.vue
@@ -41,6 +41,14 @@
           />
         </div>
       </div>
+      <div class="column flex">
+        <input
+          type="text"
+          class="p-2 mr-2 flex-grow"
+          placeholder="New Column Name"
+          v-model="newColumnName"
+          @keyup.enter="createColumn">
+      </div>
     </div>
     <div v-if="isTaskOpen" @click.self="close" class="task-bg">
       <router-view />
@@ -52,6 +60,11 @@
 import { mapState } from 'vuex'
 
 export default {
+  data () {
+    return {
+      newColumnName: ''
+    }
+  },
   computed: {
     ...mapState(['board']),
     isTaskOpen () {
@@ -71,6 +84,12 @@ export default {
         name: e.target.value
       })
       e.target.value = ''
+    },
+    createColumn () {
+      this.$store.commit('CREATE_COLUMN', {
+        name: this.newColumnName
+      })
+      this.newColumnName = ''
     },
     pickupTask (e, taskIndex, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
