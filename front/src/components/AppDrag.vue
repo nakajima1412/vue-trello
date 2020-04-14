ed19e2b8398af87fde14733d08a5dbda7accbfa1
nakajima1412 <nakajima1412@icloud.com>
2020-04-14 22:14:48 +0900

    dragコンポーネント作成

---
+++ front/src/components/AppDrag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    draggable
+    @dragstart.self="onDrag"
+    @dragover.prevent
+    @dragenter.prevent
+    >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    transferData: {
+      type: Object,
+      required: true
+    }
+  },
+  onDrag (e) {
+    e.dataTransfer.setData('payload', JSON.stringify(this.transferData))
+  }
+}
+</script>
